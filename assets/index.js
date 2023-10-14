6e503af1e1ad561a974eecccadfdd60970d392da
Srinesh-Kalimuthu <727621bec063@mcet.in>
2023-10-14 19:57:58 +0530

    scroll anime update

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,31 @@
+let sections = document.querySelectorAll('section');
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+            sec.classList.add('show-animate');
+        }
+        else {
+            sec.classList.remove('show-animate');
+        }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzdBI9OCsenePLsUHwQi5vBfztzMfN_IHWmo1AFSh16odpsQMXeXZbONykzJZlE5Zi0/exec'
 const form = document.forms['submit-to-google-sheet']
 const messageintimation = document.getElementById("messageintimation")
